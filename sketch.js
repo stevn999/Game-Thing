da453866a6da8b067cf271dc3eada1413bc631d2
stevn999 <stevenunrine@gmail.com>
2017-08-28 14:54:59 -0700

    Visual update and expirimental android functionality

--- sketch.js
+++ sketch.js
@@ -106,20 +106,20 @@ var gameClock = setInterval(function () {
  }
  lastTime = Date.now();
 }, 100);
-(function($) {
-  $.fn.nodoubletapzoom = function() {
-      $(this).bind('touchstart', function preventZoom(e) {
-        var t2 = e.timeStamp
-          , t1 = $(this).data('lastTouch') || t2
-          , dt = t2 - t1
-          , fingers = e.originalEvent.touches.length;
-        $(this).data('lastTouch', t2);
-        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
-        e.preventDefault(); // double tap - prevent the zoom
-        // also synthesize click events we just swallowed up
-        $(this).trigger('click').trigger('click');
-      });
-  };
+// (function($) {
+//   $.fn.nodoubletapzoom = function() {
+//       $(this).bind('touchstart', function preventZoom(e) {
+//         var t2 = e.timeStamp
+//           , t1 = $(this).data('lastTouch') || t2
+//           , dt = t2 - t1
+//           , fingers = e.originalEvent.touches.length;
+//         $(this).data('lastTouch', t2);
+//         if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+//         e.preventDefault(); // double tap - prevent the zoom
+//         // also synthesize click events we just swallowed up
+//         $(this).trigger('click').trigger('click');
+//       });
+//   };
 })(jQuery);
 var clock = setInterval(function () {
   if (player.health > 0) {
